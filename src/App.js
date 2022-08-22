36eb143f98834782fc2a80f077068b48ebab7b2c
umarkhan996 <mdumarkhan996@gmail.com>
2022-08-22 09:17:36 +0530

    RendringExpenses

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
+const DUMMY_EXPENSES = [{
+        id: 'e1',
+        title: 'Toilet Paper',
+        amount: 94.12,
+        date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+        id: 'e3',
+        title: 'Car Insurance',
+        amount: 294.67,
+        date: new Date(2021, 2, 28),
+    },
+    {
+        id: 'e4',
+        title: 'New Desk (Wooden)',
+        amount: 450,
+        date: new Date(2021, 5, 12),
+    },
+];
 
 
 const App = () => {
-    const expenses = [{
-            id: 'e1',
-            title: 'Toilet Paper',
-            amount: 94.12,
-            date: new Date(2020, 7, 14),
-        },
-        { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-        {
-            id: 'e3',
-            title: 'Car Insurance',
-            amount: 294.67,
-            date: new Date(2021, 2, 28),
-        },
-        {
-            id: 'e4',
-            title: 'New Desk (Wooden)',
-            amount: 450,
-            date: new Date(2021, 5, 12),
-        },
-    ];
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
 
     const addExpenseHandler = expense => {
-        console.log("In App.Js");
-        console.log(expense);
+        setExpenses((prevExpenses) => {
+            return [expense, ...prevExpenses];
+        });
     };
 
-
-
-    //Add New  Extra Expenses
     return <div >
         <
         NewExpense onAddExpense = { addExpenseHandler }
@@ -43,91 +42,6 @@ const App = () => {
     div >
 
 
-
-
-        // var count = 0;
-        // var increamentCounter = () => {
-        //     count++;
-        //     console.log(count);
-        //     document.getElementById("count").innerHTML = count;
-        // };
-
-        // THIS IS React Js code
-
-        // return React.createElement('div', {}, React.createElement('h2', {}, "Let's Get Started !"), React.createElement(Expenses, { items: expenses })); <
-        // div className = 'App' >
-        //     <
-        //     span id = "count" > { count } < /span> <
-        // button onClick = { increamentCounter } > increament < /button> < /
-        // div >
-
-
-
-
-
-
-
-        // THIS IA JSX CODE 
-        // return ( <
-        //     div >
-        //     <
-        //     h2 > Let 's get started!</h2>   <
-        //     Expenses items = { expenses }
-        //     />     < /
-        //     div >
-
-
-
-
-
-
-
-
-        // <
-        // ExpenseItem title = { expenses[0].title }
-        // amount = { expenses[0].amount }
-        // date = { expenses[0].date } >
-        // <
-        // /ExpenseItem> <
-        // ExpenseItem title = { expenses[1].title }
-        // amount = { expenses[1].amount }
-        // date = { expenses[1].date } >
-        // <
-        // /ExpenseItem> <
-        // ExpenseItem title = { expenses[2].title }
-        // amount = { expenses[2].amount }
-        // date = { expenses[2].date } >
-        // <
-        // /ExpenseItem> <
-        // ExpenseItem title = { expenses[3].title }
-        // amount = { expenses[3].amount }
-        // date = { expenses[3].date } >
-        // <
-        // /ExpenseItem> 
-        // <
-        // /
-        // div >
-
-
-        // );
-
-
-
-
-
-        // 4 time print same thing
-
-        // function App() {
-        //     return ( <
-        //         div >
-        //         <
-        //         h1 > Let 's Print 4 Time </h1>  <
-        //         ExpenseItem > < /ExpenseItem>  <
-        //         ExpenseItem > < /ExpenseItem>   <
-        //         ExpenseItem > < /ExpenseItem>  <
-        //         ExpenseItem > < /ExpenseItem>  < /
-        //         div >
-        //     );
 }
 
 export default App;
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,56 +5,7 @@ import Card from '../UI_Compo/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-    // const month = props.data.toLocaleStirng('en-US', { month: 'long' });
-    // const day = props.data.toLocaleStirng('en-US', { day: '2-digit' });
-    // const year = props.date.getFullYear();
 
-
-    // return ( <
-    //     div className = 'expense-item' >
-
-
-
-    // <
-    // div >
-    // <
-    // div > { month } < /div> <
-    // div > { year } < /div> <
-    // div > { day } < /div>  <
-    // /div>
-
-    // return ( <
-    //     div className = 'expense-item' >
-
-
-    //     <
-    //     div > { props.date.toISOString() }
-
-    //     <
-    //     /div>   <
-    //     div className = 'expense-item__description' >
-    //     <
-    //     h2 > { props.title } < /h2> <
-    //     div className = 'expense-item__price' > $ { props.amount } < /div> < /
-    //     div > <
-    //     /div>
-
-
-    //     We can use Card insteed of  main div,then we got same result 
-
-
-
-    //Event Listener 
-
-
-    // let title = props.title;
-
-    // const [title, setTitle] = useState(props.title);
-    // const clickHandler = () => {
-
-    //     setTitle('Updated!');
-    //     console.log(title);
-    // };
     return ( <
         Card className = 'expense-item' >
         <
@@ -69,58 +20,10 @@ const ExpenseItem = (props) => {
         <
         /
         Card >
-        // <
-        // button onClick = { clickHandler } > Change title < /button> 
 
 
 
 
     );
-
-
-
-    // function ExpenseItem() {
-    //     const expenseDate = new Date(2021, 7, 11);
-    //     const expenseTitle = 'INSURANCE';
-    //     const expenseAmount = 11599.56;
-    //     return ( <
-    //         div className = 'expense-item' >
-    //         <
-    //         div > { expenseDate.toISOString() } < /div> <
-    //         div className = 'expense-item__description' >
-    //         <
-    //         h2 > { expenseTitle } < /h2> <
-    //         div className = 'expense-item__price' > $ { expenseAmount } < /div>  <
-    //         /div >  <
-    //         /div>
-    //     );
-    // }
-    // export default ExpenseItem;
-
-
-
-
-
-
-    // 4 time print same thing
-
-    // function ExpenseItem() {
-    //     const expenseDate = new Date(2021, 7, 11);
-    //     const expenseTitle = 'INSURANCE';
-    //     const expenseAmount = 11599.56;
-
-    //     return ( <
-    //         div className = 'expense-item' >
-    //         <
-    //         div > { expenseDate.toISOString() } < /div> <
-    //         div className = 'expense-item__description' >
-    //         <
-    //         h2 > { expenseTitle } < /h2> <
-    //         div className = 'expense-item__price' > $ { expenseAmount } < /div> <
-    //         /div>
-
-    //         <
-    //         /div>
-    //     );
 }
 export default ExpenseItem;
