1de83a01fde8bc5d6ad0db5da1a2bdacbd1680c0
umarkhan996 <mdumarkhan996@gmail.com>
2022-08-24 05:01:43 +0530

    FilterFeature

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,35 +1,37 @@
-import React, { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI_Compo/Card';
-import ExpensesFilter from './ExpensesFilter';
-import './Expenses.css';
-
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI_Compo/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
 
 const Expenses = (props) => {
-        const [filteredYear, setFilteredYear] = useState('2020');
-        const filterChangeHandler = selectedYear => {
-            setFilteredYear(selectedYear);
-        };
-        return ( <
-            div >
+    const [filteredYear, setFilteredYear] = useState("2020");
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
+    const filteredExpnses = props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
 
-            <
-            Card className = "expenses" >
-            <
-            ExpensesFilter selected = { filteredYear }
-            onChangeFilter = { filterChangeHandler }
-            />   {
-            props.items.map((expense) => ( < ExpenseItem key = { expense.id }
-                    title = { expense.title }
-                    amount = { expense.amount }
-                    date = { expense.date }
-                    />))
-                } <
-                /
-                Card >
-                <
-                /div>
-            );
-        };
+    return ( <
+        div >
+        <
+        Card className = "expenses" >
+        <
+        ExpensesFilter selected = { filteredYear }
+        onChangeFilter = { filterChangeHandler }
+        />{" "} {
+            filteredExpnses.map((expense) => ( <
+                ExpenseItem key = { expense.id }
+                title = { expense.title }
+                amount = { expense.amount }
+                date = { expense.date }
+                />
+            ))
+        } { " " } <
+        /Card>{" "} <
+        /div>
+    );
+};
 
-        export default Expenses;
+export default Expenses;
